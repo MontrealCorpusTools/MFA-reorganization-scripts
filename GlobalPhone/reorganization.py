f6861bc59ff9b0362749feb9bda56746b6357bb2
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2017-04-01 19:32:15 -0400

    fix

--- GlobalPhone/reorganization.py
+++ GlobalPhone/reorganization.py
@@ -5,9 +5,10 @@ base_dir = '/data/mmcauliffe/aligner-output/CR'
 
 output_dir = '/media/share/corpora/GP_aligned/CR'
 
-for f in os.listdir(base_dir):
-    in_path = os.path.join(base_dir, f)
-    speaker = f.split('_')[0]
-    out_path = os.path.join(output_dir, speaker, f)
-    os.remove(out_path)
-    shutil.copyfile(in_path, out_path)
+for speaker in os.listdir(base_dir):
+    s_dir = os.path.join(base_dir, d)
+    for f in os.listdir(s_dir):
+        in_path = os.path.join(base_dir, speaker, f)
+        out_path = os.path.join(output_dir, speaker, f)
+        os.remove(out_path)
+        shutil.copyfile(in_path, out_path)
