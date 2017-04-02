e81877f43939b998cd45b06a2ebb59aa9ec20fa0
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2017-04-02 09:34:41 -0400

    Find new wav files

--- GlobalPhone/reorganization.py
+++ GlobalPhone/reorganization.py
@@ -4,12 +4,16 @@ import shutil
 base_dir = '/data/mmcauliffe/aligner-output/CR'
 
 output_dir = '/media/share/corpora/GP_aligned/CR'
+main_dir = '/media/share/corpora/GP_for_MFA/CR/files'
 
 for speaker in os.listdir(base_dir):
     s_dir = os.path.join(base_dir, speaker)
     for f in os.listdir(s_dir):
         in_path = os.path.join(base_dir, speaker, f)
-        out_path = os.path.join(output_dir, speaker, f)
-        if os.path.exists(out_path):
-            os.remove(out_path)
-        shutil.copyfile(in_path, out_path)
+        out_path = os.path.join(output_dir, speaker, f.replace('.TextGrid', '.wav'))
+        if not os.path.exists(out_path):
+            wav_path = out_path.replace(output_dir, main_dir)
+            shutil.copyfile(wav_path, out_path)
+        #if os.path.exists(out_path):
+        #    os.remove(out_path)
+        #shutil.copyfile(in_path, out_path)
