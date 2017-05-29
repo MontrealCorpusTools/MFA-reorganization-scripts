58805a8dc7a921c7d1f8e2eab7eb529ba7463fff
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2017-05-29 11:44:13 -0400

    Update for having irregular pronunciations

--- Vietnamese/generate_vn_dict.py
+++ Vietnamese/generate_vn_dict.py
@@ -6,6 +6,7 @@ from textgrid import TextGrid
 VN_dict_path = r'D:\Data\Vietnamese\lexicon_nosil.txt'
 new_dict_path = r'D:\Data\Vietnamese\vn_dict_narrow.txt'
 data_dir = r'D:\Data\Vietnamese\Brunelle_corpus'
+predefined_pronunciations = r'D:\Data\Vietnamese\irregulars.txt'
 
 glottal = False
 cao = False
@@ -47,9 +48,24 @@ def get_corpus_words():
     return words
 
 
+def add_irregulars(dictionary):
+    with open(predefined_pronunciations, 'r', encoding= 'utf8') as f:
+        for line in f:
+            line = line.strip().split()
+            if not line:
+                continue
+            word = line[0]
+            pron = line[1:]
+            if word not in dictionary:
+                dictionary[word] = []
+            dictionary[word].append(pron)
+    return dictionary
+
 if __name__ == '__main__':
     words = get_corpus_words()
     text = [sorted(words)]
     dictionary = create_dictionary(text, glottal=glottal, cao=cao, pham=pham, dialect=dialect, tokenize=tokenize,
                                    palatals=palatals)
+    if os.path.exists(predefined_pronunciations):
+        dictionary = add_irregulars(dictionary)
     save_dictionary(dictionary, new_dict_path)
