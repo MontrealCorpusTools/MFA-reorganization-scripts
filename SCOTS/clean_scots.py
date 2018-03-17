c8a6e97cce6a5a1cc4de4d7547f1d45fed85812a
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2018-03-17 14:37:26 -0400

    Update SCOTS processing

--- SCOTS/clean_scots.py
+++ SCOTS/clean_scots.py
@@ -19,58 +19,62 @@ from textgrid import TextGrid, IntervalTier
 input_dir = sys.argv[1]
 output_dir = sys.argv[2]
 
+os.makedirs(output_dir, exist_ok=True)
+
+
 def cleanText(text):
-	text = re.sub(r"<vocallex desc=\"\"", "", text)
-	text = re.sub(r"\"\"\s/>", "", text)
-	text = re.sub(r"(<unclear>|</unclear>)", "", text)
-	text = re.sub(r"(<falsestart>|-<falsestart>)", "", text)
-	text = re.sub(r"(<trunc>|-</trunc>)", "", text)
-	text = re.sub(r"<note>.*</note>", "", text)
+    text = re.sub(r"<vocallex desc=\"\"", "", text)
+    text = re.sub(r"\"\"\s/>", "", text)
+    text = re.sub(r"(<unclear>|</unclear>)", "", text)
+    text = re.sub(r"(<falsestart>|-<falsestart>)", "", text)
+    text = re.sub(r"(<trunc>|-</trunc>)", "", text)
+    text = re.sub(r"<note>.*</note>", "", text)
 
-	text = re.sub(r"<vocal desc=\"\".*/>", "", text)
-	text = re.sub(r"<censored type=\"\".*\"\" >.*</censored>", "beep_sound", text)
-	text = re.sub(r"censored type.*censored", "beep_sound", text)
-	text = re.sub(r"<gap reason=\"\".*\"\" \s/>", "", text)
-	text = re.sub(r"vocallex desc", "", text)
-	text = re.sub(r"(vocal desc.*\s|vocal desc.*\n|vocal desc.*\")", " ", text)
-	text = re.sub(r"<vocal desc=\"\".*/>", "", text)
-	text = re.sub(r"event desc.*\s", "", text)
-	text = re.sub(r"(gap reason.*\s|gap reason.*\n|gap reason.*\")", " ", text)
-	text = re.sub(r"falsestart", "", text)
-	text = re.sub(r"<br />", "", text)
+    text = re.sub(r"<vocal desc=\"\".*/>", "", text)
+    text = re.sub(r"<censored type=\"\".*\"\" >.*</censored>", "beep_sound", text)
+    text = re.sub(r"censored type.*censored", "beep_sound", text)
+    text = re.sub(r"<gap reason=\"\".*\"\" \s/>", "", text)
+    text = re.sub(r"vocallex desc", "", text)
+    text = re.sub(r"(vocal desc.*\s|vocal desc.*\n|vocal desc.*\")", " ", text)
+    text = re.sub(r"<vocal desc=\"\".*/>", "", text)
+    text = re.sub(r"event desc.*\s", "", text)
+    text = re.sub(r"(gap reason.*\s|gap reason.*\n|gap reason.*\")", " ", text)
+    text = re.sub(r"falsestart", "", text)
+    text = re.sub(r"<br />", "", text)
 
-	text = re.sub(r"(?<![a-zA-Z])'(?![a-zA-Z])", "", text)	# Deletes ', except when surrounded by alphabet
-	text = re.sub(r"(?<![a-zA-Z])-(?![a-zA-Z])", "", text)	# Deletes -, except when surrounded by alphabet, eg. keeps "twenty-seventh"
-	text = re.sub(ur"[^a-zA-Z\s'_-]", "", text)
-	text = re.sub(r"(^\'[a-zA-Z]|\s\'[a-zA-Z]|[a-zA-Z]\'\s|[a-zA-Z]\'\n|[a-zA-Z]\'$)", "", text)	
+    text = re.sub(r"(?<![a-zA-Z])'(?![a-zA-Z])", "", text)  # Deletes ', except when surrounded by alphabet
+    text = re.sub(r"(?<![a-zA-Z])-(?![a-zA-Z])", "",
+                  text)  # Deletes -, except when surrounded by alphabet, eg. keeps "twenty-seventh"
+    text = re.sub(r"[^a-zA-Z\s'_-]", "", text)
+    text = re.sub(r"(^\'[a-zA-Z]|\s\'[a-zA-Z]|[a-zA-Z]\'\s|[a-zA-Z]\'\n|[a-zA-Z]\'$)", "", text)
 
-	text = text.split()
-	for item in text:
-		if len(item) == 1 and item in "bcdefghijklmnpqrstuvwxyz":
-			item = ""
-	text = " ".join(text)
-	return text
+    text = text.split()
+    #for item in text:
+    #    if len(item) == 1 and item in "bcdefghijklmnpqrstuvwxyz":
+    #        item = ""
+    text = " ".join(text)
+    return text
 
 
 if __name__ == '__main__':
-	# Loop through all textgrids
-	for file in os.listdir(input_dir):
-		if file.endswith(".TextGrid"):
-			print "Processing " + file + "..."
-			filePath = os.path.join(input_dir, file)
-			tg = TextGrid()
-			tg.read(filePath)	# Read into a textgrid
-		else:
-			continue
+    # Loop through all textgrids
+    for file in os.listdir(input_dir):
+        if file.endswith(".TextGrid"):
+            print("Processing " + file + "...")
+            filePath = os.path.join(input_dir, file)
+            tg = TextGrid()
+            tg.read(filePath)  # Read into a textgrid
+        else:
+            continue
 
-		# Clean the text
-		for tier in tg.tiers:
-			for interval in tier:
-				#print repr(interval.mark)
-				interval.mark = cleanText(interval.mark)
-				if interval.mark != "":
-					print interval.mark
+        # Clean the text
+        for tier in tg.tiers:
+            for interval in tier:
+                # print repr(interval.mark)
+                interval.mark = cleanText(interval.mark)
+                if interval.mark != "":
+                    print(interval.mark)
 
-		# Write to file
-		outputPath = os.path.join(output_dir, file)
-		tg.write(outputPath)
+        # Write to file
+        outputPath = os.path.join(output_dir, file)
+        tg.write(outputPath)
